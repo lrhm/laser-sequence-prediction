fb68d9342a319e7bec922a426f127f8a88926f50
GiulioZani <giulio.zani@gmail.com>
2022-04-11 14:54:34 +0200

    fix

--- lstm_model.py
+++ lstm_model.py
@@ -5,17 +5,17 @@ import ipdb
 
 class VanillaLSTM(t.nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
-        super(VanillaLSTM, self).__init__()
+        super().__init__()
         self.hidden_size = hidden_size
         self.lstm = t.nn.LSTM(input_size, hidden_size)
         self.act = t.nn.Tanh()
         # self.linear = t.nn.Linear(hidden_size, output_size)
 
-    def forward(self, input, hidden):
-        output, hidden = self.lstm(input, hidden)
+    def forward(self, input):
+        output, _ = self.lstm(input)
         # tanh on output
         output = self.act(output)
-        return output, hidden
+        return output 
 
     def init_hidden(self):
         return t.autograd.Variable(t.zeros(1, 1, self.hidden_size))
